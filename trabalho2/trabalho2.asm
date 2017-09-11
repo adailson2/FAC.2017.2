c3cf365b89785df75d8822c1a38e3592f1e6868e
Vitor Nere <vitor.nere@hotmail.com>
2017-09-11 13:39:42 -0300

    Adicionado extensão do arquivo e comentários

---
+++ trabalho2/trabalho2.asm
@@ -0,0 +1,36 @@
+.data
+     text:  .asciiz "Insira um número primo: "
+.text
+
+ler_inteiro:
+    # Print input messeger
+    li $v0, 4           # system call code for print string in $a0
+    la $a0, text        # load address of string to be printed into $a0
+    syscall             # call operation system
+
+    # User input value
+    li $v0, 5           # system call code for read integer = 5
+    li $v1, 5           # ????
+    syscall
+
+    # Move value to t0
+    move $t0, $v0       # move value to from $v0 to $t0 in wich is used to be print integer
+    
+
+    # Print value
+    li $v0, 1           # system call code for print integer = 1
+    move $a0, $t0       # move value to from $t0 to $a0 in wich is used to be print integer
+    syscall             # call operation
+    
+    # End program
+    li $v0, 10          # system call code for terminate program = 10
+    syscall             # call operation
+eh_primo:
+    
+calc_inverso:
+
+imprime_erro:
+
+imprime_saida:
+
+
