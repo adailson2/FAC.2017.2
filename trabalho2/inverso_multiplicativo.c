b46ce734b8e606f065caa35dc1b4713a65becb95
Vitor Nere <vitor.nere@hotmail.com>
2017-09-13 14:33:43 -0300

    Feito eh_primo em C

--- trabalho2/inverso_multiplicativo.c
+++ trabalho2/inverso_multiplicativo.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
+int eh_primo(int p) {
+    if(p < 3 && p >= 1) 
+        return 1; // 
+    else if(p<1)
+        return 0;
+    else {
+        i = p-2;
+        do {
+            if(p%i == 0)
+                return 0;
+
+            i = i-2;    
+        } while(i != 1);
+    }
+
+    return 1;
+}
+
 int main() {
     int num1, num2, i, mod;
 
     scanf("%d", &num1);
     scanf("%d", &num2);
+
+    if(eh_primo(num1) != 1)
+        printf("O modulo nao eh primo.\n");
 
     i = num1 - 1;
 
@@ -13,10 +34,8 @@ int main() {
         mod = mod % num1;
         if(mod == 1) {
             printf("O inverso multiplicativo é %d.\n", i);
-            return 0;
         }
     }
-    printf("O modulo nao eh primo.\n");
 
     return 0;
 }
--- trabalho2/trabalho2.asm
+++ trabalho2/trabalho2.asm
@@ -1,5 +1,6 @@
 .data
      text:  .asciiz "Insira um número primo: "
+     text2:  .asciiz "Insira um número inteiro: "
 .text
 
 ler_inteiro:
@@ -10,27 +11,37 @@ ler_inteiro:
 
     # User input value
     li $v0, 5           # system call code for read integer = 5
-    li $v1, 5           # ????
     syscall
 
     # Move value to t0
     move $t0, $v0       # move value to from $v0 to $t0 in wich is used to be print integer
-    
 
-    # Print value
-    li $v0, 1           # system call code for print integer = 1
-    move $a0, $t0       # move value to from $t0 to $a0 in wich is used to be print integer
-    syscall             # call operation
+    # Print input messeger
+    li $v0, 4           # system call code for print string in $a0
+    la $a0, text2        # load address of string to be printed into $a0
+    syscall             # call operation system
+
+    # User input value
+    li $v0, 5           # system call code for read integer = 5
+    syscall
+
+    # Move value to t0
+    move $t1, $v0       # move value to from $v0 to $t0 in wich is used to be print integer
     
     # End program
     li $v0, 10          # system call code for terminate program = 10
     syscall             # call operation
+
 eh_primo:
     
 calc_inverso:
+
 
 imprime_erro:
 
 imprime_saida:
 
-
+    # Print value
+    li $v0, 1           # system call code for print integer = 1
+    move $a0, $t0       # move value to from $t0 to $a0 in wich is used to be print integer
+    syscall             # call operation
